de8a7a017f67cdd32be603649d0f16217b3c1fa8
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-05 21:17:35 +0500

    Task2

---
+++ Tyuiu.RubanovEO.Sprint4.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.RubanovEO.Sprint4.Task2.V6.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
