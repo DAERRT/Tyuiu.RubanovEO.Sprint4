a8f91c0387bd95f5c1b7b6cdee21471ab2bd2ba6
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-11-10 22:21:18 +0500

    task4

--- Tyuiu.RubanovEO.Sprint4.Task4.V25.Test/DataServiceTest.cs
+++ Tyuiu.RubanovEO.Sprint4.Task4.V25.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.RubanovEO.Sprint4.Task4.V25.Test
         public void Test1()
         {
             DataService ds = new DataService();
-            Assert.That(ds.Calculate(new int[5, 5] {{3, 2, 4, 4, 4 },{2, 6, 6, 3, 2 },{4, 2, 5, 5, 3 },{5, 5, 6, 4, 3 },{2, 6, 5, 6, 2 }}), Is.EqualTo(24));
+            Assert.That(ds.Calculate(new int[5, 5] { { 3, 2, 4, 4, 4 }, { 2, 6, 6, 3, 2 }, { 4, 2, 5, 5, 3 }, { 5, 5, 6, 4, 3 }, { 2, 6, 5, 6, 2 } }), Is.EqualTo(24));
         }
     }
 }
